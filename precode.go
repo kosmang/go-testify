e68a1274bc2f22b956a69da1028586aa31eb7dbd
kosmang <kosman2002@mail.ru>
2024-03-26 23:04:45 +0500

    Second commit

--- precode.go
+++ precode.go
@@ -38,8 +38,16 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 		count = len(cafe)
 	}
 
-	answer := strings.Join(cafe[:count], ",")
+	answer := strings.Join(cafe[:count], ", ")
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
+}
+
+func main() {
+	http.HandleFunc(`/cafe`, mainHandle)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
